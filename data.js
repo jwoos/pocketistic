a0e65f34dabb5c257b128c73eadf73a337a699a0
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-06-09 00:41:45 +0000

    fixed

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const constants = {
 	consumerKey: '51787-0f16e59e8e3c7ee3d8c777c4',
 	apiBase: 'https://getpocket.com/v3/',
 	pgConnection: process.env.DATABASE_URL || 'postgres://ubuntu:123456789@localhost/pocketistic',
-	port: process.env.PORT || 8080;
+	port: process.env.PORT || 8080
 };
 
 module.exports = constants;
