6a1291b36411fd905103cac1cb44efc37445287b
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-06-11 09:37:12 -0400

    added valid check for env

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const constants = {
 	apiAccess: 'https://getpocket.com/v3/oauth/authorize',
 	pgConnection: process.env.DATABASE_URL || 'postgres://postgres:123456789@localhost/pocketistic',
 	port: process.env.PORT || 8080,
-	url: process.env.HEROKU ? 'https://sheltered-badlands-26515.herokuapp.com' : '192.168.1.20:8080'
+	url: process.env.NODE_ENV ? 'https://sheltered-badlands-26515.herokuapp.com' : '192.168.1.20:8080'
 };
 
 module.exports = constants;
