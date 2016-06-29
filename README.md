f2eaddf0b7e832104f9c4f7fb3225a12590f8c60
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-06-28 22:48:05 -0400

    sequelize

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
 # Pocketistic
 ## To do
-- sessions: https://github.com/expressjs/session
-	- postgres: https://github.com/voxpelli/node-connect-pg-simple
-	- PROBABLY WON'T NEED THIS: Look into cookies: http://expressjs.com/en/4x/api.html#res.cookie
-		- Work on storing into local storage as well if possible
-- http://docs.sequelizejs.com/en/latest/
-	- model file for sessions
-	- cache all data
-- default role on postgres
-	- create a user for my user?
+- migrations
+- fix up models
--- models/session.js
+++ models/session.js
@@ -1,33 +1,53 @@
 'use strict';
 
-const pg = require('pg');
+const Sequelize = require('sequelize');
+
+const sequelize = require('../db');
 const data = require('../data');
 
-if (process.env.NODE_ENV === 'production') {
-	pg.defaults.ssl = true;
-}
-
-pg.connect(data.pgConnection, function(err, client, done) {
-	if (err) {
-		return console.log(err);
+let Session = sequelize.define('session', {
+	sid: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		primaryKey: true
+	},
+	sess: {
+		type: Sequelize.JSON,
+		allowNull: false
+	},
+	expire: {
+		type: Sequelize.DATE(6),
+		allowNull: false
 	}
-
-	let query = client.query(`
-		CREATE TABLE "session" (
-		  "sid" varchar NOT NULL COLLATE "default",
-			"sess" json NOT NULL,
-			"expire" timestamp(6) NOT NULL
-		)
-		WITH (OIDS=FALSE);
-		ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
-	`);
-
-	query.on('error', function(err) {
-		console.log(err);
-	});
-
-	query.on('end', function() {
-		console.log('Created session table');
-		done();
-	});
+}, {
+	timestamps: false,
+	freezeTableName: true,
+	tableName: 'session'
 });
+
+/*
+ *pg.connect(data.pgConnection, function(err, client, done) {
+ *  if (err) {
+ *    return console.log(err);
+ *  }
+ *
+ *  let query = client.query(`
+ *    CREATE TABLE "session" (
+ *      "sid" varchar NOT NULL COLLATE "default",
+ *      "sess" json NOT NULL,
+ *      "expire" timestamp(6) NOT NULL
+ *    )
+ *    WITH (OIDS=FALSE);
+ *    ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+ *  `);
+ *
+ *  query.on('error', function(err) {
+ *    console.log(err);
+ *  });
+ *
+ *  query.on('end', function() {
+ *    console.log('Created session table');
+ *    done();
+ *  });
+ *});
+ */
