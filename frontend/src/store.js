15994726f6e664baaa75e3acc74d0c88893005c8
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-05-06 05:43:09 +0000

    source scripts from npm

---
+++ frontend/src/store.js
@@ -0,0 +1,22 @@
+import {applyMiddleware, createStore, compose} from 'redux';
+import {combineReducers} from 'redux-immutable';
+import createLogger from 'redux-logger';
+import Immutable from 'immutable';
+
+import reducers from './reducers';
+
+const reducer = combineReducers(reducers);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+	reducer,
+	Immutable.Map({}),
+	composeEnhancers(
+		applyMiddleware(
+			createLogger()
+		)
+	)
+);
+
+export default store;
